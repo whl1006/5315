8f856a765fbeae3186da0dbc52d151775fcefd7c
richard <wangh15@seattleu.edu>
2018-04-24 12:40:06 +0900

    hw2 copied more files

---
+++ hw2data.js
@@ -0,0 +1,12 @@
+{
+  "data": [
+    {
+      "name": "James Bond",
+      "email": "newmail@email.com"
+    },
+    {
+      "name": "Brielle Williamson",
+      "email": "jbond@su.edu"
+    }
+  ]
+}
