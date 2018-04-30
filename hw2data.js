53eca0ecbfd3cd98ae7803822d5f3052ed87bbce
richard <wangh15@seattleu.edu>
2018-04-30 09:17:34 +0900

    hw2 copied files

--- hw2data.js
+++ hw2data.js
@@ -2,11 +2,17 @@
   "data": [
     {
       "name": "James Bond",
-      "email": "newmail@email.com"
+      "email": "newmail@email.com",
+      "position": "detective",
+      "location": "Moscow",
+      "Company": "Self Employed"
     },
     {
-      "name": "Brielle Williamson",
-      "email": "jbond@su.edu"
+      "name": "Stephen Curry",
+      "email": "currys@gsw.com",
+      "position": "detective",
+      "location": "Moscow",
+      "Company": "Self Employed"
     }
   ]
 }
