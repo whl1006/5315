fcef88e92de9dfc471d851c2b2ddce83a4c651ac
richard <wangh15@seattleu.edu>
2018-04-30 10:06:25 +0900

    hw2 copied files

--- hw2data.js
+++ hw2data.js
@@ -3,16 +3,122 @@
     {
       "name": "James Bond",
       "email": "newmail@email.com",
-      "position": "detective",
+      "job": "detective",
       "location": "Moscow",
-      "company": "Self Employed"
+      "company": "Self Employed",
+      "age:65"
     },
     {
       "name": "Stephen Curry",
       "email": "currys@gsw.com",
-      "position": "detective",
-      "location": "Moscow",
-      "company": "Self Employed"
-    }
+      "job": "Basketball Player",
+      "location": "San Francisco",
+      "company": "Golden State Warriors",
+      "age:30"
+    },
+    {
+      "name": "Paul Pogba",
+      "email": "AgentP@manutd.com",
+      "job": "Soccer Player",
+      "location": "Manchester",
+      "company": "Manchester United",
+      "age:25"
+    },
+    {
+      "name": "Cristiano Ronaldo",
+      "email": "CR7GOAT@realmadrid.com",
+      "job": "Soccer Player",
+      "location": "Madrid",
+      "company": "Real Madrid",
+      "age:33"
+    },
+    {
+      "name": "Roger Federer",
+      "email": "rogerfederer@atp.com",
+      "job": "tennis Player",
+      "location": "Bern",
+      "company": "Self Employed",
+      "age:36"
+    },
+    {
+      "name": "Sebastian Vettel",
+      "email": "vettels@ferrari.com",
+      "job": "Formula 1 Driver",
+      "location": "Berlin",
+      "company": "Ferrari",
+      "age:30"
+    },
+    {
+      "name": "Russell Wilson",
+      "email": "dangeruss@seahawks.com",
+      "job": "Football Player",
+      "location": "Seattle",
+      "company": "Seattle Seahawks",
+      "age:29"
+    },
+    {
+      "name": "Adam Silver",
+      "email": "silvera@NBA.com",
+      "job": "Commissioner",
+      "location": "New York",
+      "company": "National Basketball Association",
+      "age:56"
+    },
+    {
+      "name": "Xuecheng Gao",
+      "email": "weixiao@worldelite.com",
+      "job": "LOL Pro Player",
+      "location": "Shanghai",
+      "company": "World Elite Club",
+      "age:25"
+    },
+    {
+      "name": "Zhuojun Feng",
+      "email": "fzzf@worldelite.com",
+      "job": "LOL Pro Player",
+      "location": "Bengbu",
+      "company": "World Elite Club",
+      "age:25"
+    },
+    {
+      "name": "Anny Chen",
+      "email": "chenx5@seattleu.edu",
+      "job": "Library Assistant",
+      "location": "Seattle",
+      "company": "Seattle University",
+      "age:20"
+    },
+    {
+      "name": "Johnathan Levell",
+      "email": "jlevell@countonthat.com",
+      "job": "Accountant",
+      "location": "Seattle",
+      "company": "Count on That",
+      "age:49"
+    },
+    {
+      "name": "Lily Sun",
+      "email": "Lilys@KWMallesons.com",
+      "job": "Lawyer",
+      "location": "Los Angeles",
+      "company": "King & Wood Mallesons",
+      "age:24"
+    },
+    {
+      "name": "Joanne Shea",
+      "email": "shea4joa@gmail.com",
+      "job": "Pastor",
+      "location": "Singapore",
+      "company": "New Hope International Church",
+      "age:41"
+    },
+    {
+      "name": "Yuki Kashiwagi",
+      "email": "mayuyu@aks.com",
+      "job": "Singer",
+      "location": "Kagoshima",
+      "company": "AKS",
+      "age:27"
+    },
   ]
 }
